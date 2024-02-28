32d5464dceec1ba1951ff7bd9786d52f9d61dca0
mikaa2001 <mikaa2001@gmail.com>
2024-02-28 09:50:55 +0500

    Subsequence

---
+++ Subsequence.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Subsequence
+{
+	internal class Subsequence
+	{
+		static void Main(string[] args)
+		{
+			int starnNumberFromSequence = 5;
+			int step = 7;
+			int maxNumberFromSequence = 96;
+
+			for (int i = starnNumberFromSequence; i <= maxNumberFromSequence; i += step)
+			{
+				Console.Write(i + " ");
+			}
+			Console.WriteLine();
+		}
+	}
+}
